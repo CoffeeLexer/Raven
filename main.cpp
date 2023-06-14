f7e2ceaf4e09e1f8878ff070a19aa4c96aa18dc9
T4rt4ru5 <vitkuseduardas@gmail.com>
2023-06-14 10:15:09 +0300

    Integrate GLFW

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout << "Start of execution\n";
+
+    glfwInit();
+    GLFWwindow *window = glfwCreateWindow(250, 250, "Window", nullptr, nullptr);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+    
+    std::cout << "End   of execution\n";
+
     return 0;
 }
