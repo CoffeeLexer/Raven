3cecf20bf4f16d0e8647c5a0333d4e908d9fe4e0
T4rt4ru5 <vitkuseduardas@gmail.com>
2023-06-14 10:15:09 +0300

    Integrate glad (OpenGL)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -7,13 +8,26 @@ int main()
     std::cout << "Start of execution\n";
 
     glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     GLFWwindow *window = glfwCreateWindow(250, 250, "Window", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL(glfwGetProcAddress);
 
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+
+        glClearColor(25.0f / 256.0f, 36.0f / 256.0f, 52.0f / 256.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
     }
-    
+
     std::cout << "End   of execution\n";
 
     return 0;
